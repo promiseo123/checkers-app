bc07a4484299c741a02d9b2398cad19e0f3da375
Jack Thomas <jbt5011@rit.edu>
2020-09-26 15:29:28 -0400

    Added support for GetSigninRoute

--- src/main/java/com/webcheckers/ui/WebServer.java
+++ src/main/java/com/webcheckers/ui/WebServer.java
@@ -53,6 +53,7 @@ public class WebServer {
    * The URL pattern to request the Home page.
    */
   public static final String HOME_URL = "/";
+  public static final String SIGNIN_URL = "/sign-in";
 
   //
   // Attributes
@@ -138,6 +139,9 @@ public class WebServer {
 
     // Shows the Checkers game Home page.
     get(HOME_URL, new GetHomeRoute(templateEngine));
+
+    //Shows Sign-In page
+    get(SIGNIN_URL,new GetSigninRoute(templateEngine));
 
     //
     LOG.config("WebServer is initialized.");
