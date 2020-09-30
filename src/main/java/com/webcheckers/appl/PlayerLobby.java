55c54bafbec433e716aea7877c926efb3b4af1c6
Jack Thomas <jbt5011@rit.edu>
2020-09-29 20:07:12 -0400

    Added skeleton of signin code

--- src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -1,6 +1,18 @@
 package com.webcheckers.appl;
 
-public class PlayerLobby {
+import com.webcheckers.model.Player;
 
+public class PlayerLobby {
+    public Player signIn(String name){
+        /*
+        Evaluates given playername and returns Player if valid
+         */
+        if (name.matches("[A-Za-z0-9]+")){ //String consists of at least 1 alphanumeric character.
+            Player newplayer = new Player(name);
+            //$nameofarraylist.add(newPlayer);
+            return newplayer;
+        }
+        else return null;
+    }
 
 }
