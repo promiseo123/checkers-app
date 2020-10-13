9369fdb8c94cb9747048cd9f2091dbe7e1f78206
Promise Omiponle <poo9724@g.rit.edu>
2020-10-12 23:22:28 -0400

    PlayerLobbyTest fully tests correctly. PostSigninRouteTest sort of done, but still has issues. Will try to figure out what is wrong.

--- src/test/java/com/webcheckers/appl/PlayerLobbyTest.java
+++ src/test/java/com/webcheckers/appl/PlayerLobbyTest.java
@@ -1,7 +1,6 @@
 package com.webcheckers.appl;
 
 import com.webcheckers.model.Player;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -10,11 +9,15 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test class for PlayerLobby functionality
+ *
+ * @author Promise Omipone, poo9724
+ */
 @Tag("Application-tier")
 public class PlayerLobbyTest {
 
-    private PlayerLobby lobby;
-
+    private PlayerLobby lobby=new PlayerLobby();
 
     @Test
     public void testSignIn_Invalid_Name() {
@@ -45,7 +48,12 @@ public class PlayerLobbyTest {
 
     @Test
     public void testGetOthers() {
+        lobby.getPlayers().add(new Player("gamer"));
+        lobby.getPlayers().add(new Player("poggers"));
+        lobby.getPlayers().add(new Player("codslayer"));
         List<Player> testOtherPlayers = new ArrayList<>();
+        testOtherPlayers.add(new Player("poggers"));
+        testOtherPlayers.add(new Player("codslayer"));
         lobby.getPlayer("gamer").playing();
         assertEquals(testOtherPlayers, lobby.getOtherPlayers("gamer"));
     }
@@ -61,6 +69,7 @@ public class PlayerLobbyTest {
 
     @Test
     public void testAssignPlayerToGame() {
+        lobby.getPlayers().add(new Player("gamer"));
         lobby.assignPlayerToGame("gamer", "game1");
         assertEquals("game1", lobby.getPlayer("gamer").getGameID());
     }
@@ -75,6 +84,7 @@ public class PlayerLobbyTest {
 
     @Test
     public void testMarkAsPlaying() {
+        lobby.getPlayers().add(new Player("gamer"));
         lobby.markPlayerAsPlaying("gamer");
         assertTrue(lobby.getPlayer("gamer").isPlaying());
     }
@@ -89,6 +99,7 @@ public class PlayerLobbyTest {
 
     @Test
     public void testMarkAsDone() {
+        lobby.getPlayers().add(new Player("gamer"));
         lobby.markPlayerAsDonePlaying("gamer");
         assertFalse(lobby.getPlayer("gamer").isPlaying());
         assertEquals("", lobby.getPlayer("gamer").getGameID());
@@ -96,6 +107,7 @@ public class PlayerLobbyTest {
 
     @Test
     public void testMarkWithColor() {
+        lobby.getPlayers().add(new Player("gamer"));
         lobby.markPlayerWithColor("gamer", Player.COLOR.RED);
         assertEquals(Player.COLOR.RED, lobby.getPlayer("gamer").getColor());
     }
--- src/test/java/com/webcheckers/ui/PostSigninRouteTest.java
+++ src/test/java/com/webcheckers/ui/PostSigninRouteTest.java
@@ -1,12 +1,22 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.appl.PlayerLobby;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import spark.ModelAndView;
 import spark.Request;
 import spark.Session;
 import spark.TemplateEngine;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 @Tag("UI-tier")
 class PostSigninRouteTest {
+
 
     //Unit under Test
     private PostSigninRoute CuT;
@@ -14,4 +24,34 @@ class PostSigninRouteTest {
     private Request request;
     private Session session;
     private TemplateEngine engine;
+
+    @BeforeEach
+    public void setup() {
+        //mock dependencies
+        request = mock(Request.class);
+        session = mock(Session.class);
+        when(request.session()).thenReturn(session);
+        engine = mock(TemplateEngine.class);
+        //create CuT
+        CuT = new PostSigninRoute(new PlayerLobby(),engine);
+    }
+
+    @Test
+    void ctestInvalidParams(){
+        //test for invalid initialization
+        assertThrows(IllegalArgumentException.class,()->{new PostSigninRoute(null,null);});
+    }
+
+    @Test
+    void handleTest() {
+        TemplateEngineTester testHelper = new TemplateEngineTester();
+        when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+
+        //verify view model
+        testHelper.assertViewModelExists();
+        testHelper.assertViewModelIsaMap();
+
+        testHelper.assertViewModelAttribute("Title", "Sign In");
+        testHelper.assertViewName("signin.ftl");
+    }
 }
